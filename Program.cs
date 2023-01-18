1763d34e369a6f1fd911ff6e9db7b17fe91edf5f
Justin Simon <justinsimon@Justins-MBP.lan>
2023-01-17 20:28:03 -0800

    Finished Assignment

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace OperatorExercise
+{
+    public class Program
+    {
+
+
+
+
+
+
+       public static void Main(string[] args)
+        {
+ int a = 17;
+ int b = 4;
+
+ int q = a/b;
+ int r = a%b;
+Console.WriteLine($"{a} / {b} is {q} remainder {r}"); 
+//end of exercise 1
+
+Console.WriteLine("What is the radius?");
+
+ 
+  double radius = double.Parse(Console.ReadLine());
+double AreaOfCircle = Math.PI * (radius * radius);
+
+ 
+ 
+ Console.WriteLine($"The area of the circle with a radius of {radius} is {AreaOfCircle}");
+ }
+
+
+
+
+
+
+
+    }
+
+
+}
